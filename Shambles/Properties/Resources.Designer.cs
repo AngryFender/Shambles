e21f5f69e9b1f8a8b0f94e564c016d581da654e0
AngryFender <teaBiscuitPerson@gmail.com>
2025-12-07 22:23:07 +0000

    Changed the Binary name to Shambles and added some configuration options

--- Shambles/Properties/Resources.Designer.cs
+++ Shambles/Properties/Resources.Designer.cs
@@ -39,7 +39,7 @@ namespace Shambles.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Peeky_Blinkers.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Shambles.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
